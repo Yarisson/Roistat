c89e967ec45c1a7aad8acb3153769189511286f8
yarisson <gemini8811@gmail.com>
2021-02-24 05:41:26 +0300

    Initial dist subtree commit

---
+++ dist/precache-manifest.f733ba3e34a84b2a7fbe9e80f780c2ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3edb813dcde57c769755",
+    "url": "/css/app.4b79cdd4.css"
+  },
+  {
+    "revision": "6bf77488544374cd52e4",
+    "url": "/css/chunk-vendors.18ec59e3.css"
+  },
+  {
+    "revision": "20c4f970815706188b977214e03127f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fffe2877126efa5e2bf6",
+    "url": "/js/about.7318a9ec.js"
+  },
+  {
+    "revision": "3edb813dcde57c769755",
+    "url": "/js/app.8f5bb819.js"
+  },
+  {
+    "revision": "6bf77488544374cd52e4",
+    "url": "/js/chunk-vendors.591978c1.js"
+  },
+  {
+    "revision": "0bbf495bf6d2cfda318a0d4d25853844",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
