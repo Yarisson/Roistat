00845c68fce9897e42ad294bb7b380ea97a90328
yarisson <gemini8811@gmail.com>
2021-02-24 05:36:27 +0300

    Initial dist subtree commit

---
+++ dist/precache-manifest.b961845c1a3ebf6ee8545d465b5aba6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d5aad86f0f8f77a756d",
+    "url": "/roistat/css/app.4b79cdd4.css"
+  },
+  {
+    "revision": "3f19e57ec769972a47ad",
+    "url": "/roistat/css/chunk-vendors.18ec59e3.css"
+  },
+  {
+    "revision": "8044ec764d4eb64ce535dbbf30a7fc31",
+    "url": "/roistat/index.html"
+  },
+  {
+    "revision": "4d5aad86f0f8f77a756d",
+    "url": "/roistat/js/app.7a5ecf4c.js"
+  },
+  {
+    "revision": "3f19e57ec769972a47ad",
+    "url": "/roistat/js/chunk-vendors.22eacd25.js"
+  },
+  {
+    "revision": "0bbf495bf6d2cfda318a0d4d25853844",
+    "url": "/roistat/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/roistat/robots.txt"
+  }
+]);
